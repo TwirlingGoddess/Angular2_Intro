72207b28b9feb9647722787454a9b18fcd18df3c
Lee Graham <graham.la3@gmail.com>
2019-04-12 20:19:20 -0600

    Finish walkthrough of Angular2 beginners tutorial

---
+++ app/auto-grow.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, Renderer } from 'angular2/core'
+
+@Directive({
+    selector: '[autGrow]',
+    host: {
+        '(focus)': 'onFocus()',
+        '(blur)': 'onBlur()'
+    }
+})
+
+export class AutoGrowDirective {
+    constructor(private el: ElementRef, private renderer: Renderer){
+    }
+    onFocus(){
+        this.renderer.setElementStyle(this.el, 'width', '300');
+    }
+    onBlur(){
+        this.renderer.setElementStyle(this.el, 'width', '100');
+    }
+}
